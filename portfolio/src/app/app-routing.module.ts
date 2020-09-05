2350debb875e24f72e94c4c3db13c3c0743c93bf
sharadroy-choudhury <sharad.src42@gmail.com>
2020-09-05 16:06:09 +0100

    portflio bio update

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import {APP_BASE_HREF} from '@angular/common';
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+ 
+  providers: [{provide: APP_BASE_HREF, useValue : '/' }]
 })
 export class AppRoutingModule { }
