828d8daf7d9c1c516848b4d4ba6616a372da0494
sharadroy-choudhury <sharad.src42@gmail.com>
2020-09-06 01:39:51 +0100

    stable phase commit

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
